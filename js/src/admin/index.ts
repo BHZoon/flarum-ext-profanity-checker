d2cd16c2916cf94ebd289dc3075bb448dddbec55
Burak Hüryasar <huryasarb@yahoo.de>
2025-10-29 15:15:09 +0100

    adding permissions

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -4,5 +4,13 @@ import ProfanityCheckerSettingsModal from './components/ProfanityCheckerSettings
 app.initializers.add('bhzoon-profanity-checker', () => {
   app.extensionData
     .for('bhzoon-profanity-checker')
-    .registerPage(ProfanityCheckerSettingsModal);
+    .registerPage(ProfanityCheckerSettingsModal)
+    .registerPermission(
+        {
+          icon: 'fas fa-shield-alt',
+          label: app.translator.trans('bhzoon-profanity-checker.admin.permissions.bypass_label'),
+          permission: 'bhzoon.profanity.bypass',
+        },
+        'moderate'
+      );
 });
